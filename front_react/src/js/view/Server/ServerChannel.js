cbc069436c98140f70423e36243a302786d20d80
fdsf53451001 <35889113+fdsf53451001@users.noreply.github.com>
2021-06-16 14:30:35 +0800

    work 06/16

--- front_react/src/js/view/Server/ServerChannel.js
+++ front_react/src/js/view/Server/ServerChannel.js
@@ -4,9 +4,12 @@ import { withStyles } from '@material-ui/core/styles';
 var GRAY_1 = '#202225';
 var GRAY_2 = '#2F3136';
 var GRAY_3 = '#36393F';
+var GRAY_WORD = '#999B9F';
 
 const useStyles = (theme) => ({
-  
+    serverChannelItem:{
+        
+    },
 });
 
 class ServerPage extends React.Component{
@@ -25,20 +28,37 @@ class ServerPage extends React.Component{
       return(
         <div className='serverChannel'>
             <div className='serverChannelItem'>
-                <typography
-                    component="h4" 
-                    variant="h4"
-                    style={{color:'white',margin:'15pt 15pt'}}
-                    > <strong># 幹話R你各位</strong>
+                <typography variant="caption" display="block" gutterBottom
+                    style={{color: GRAY_WORD,position:'relative',margin:'15pt 15pt'}}
+                    >v 文字頻道
                 </typography>
+                <br/>
+                <div className='serverChannelRoom'>
+                    <typography
+                        component="h3" 
+                        variant="h3"
+                        style={{color:'white',margin:'15pt 15pt'}}
+                        > <strong># 幹話R你各位</strong>
+                    </typography>
+                </div>
+                <br/>                
             </div>            
             <div className='serverChannelItem'>
-                <typography
-                    component="h4" 
-                    variant="h4"
-                    style={{color:'white',margin:'15pt 15pt'}}
-                    ># 一般
+                <typography variant="caption" display="block" gutterBottom
+                    style={{color: GRAY_WORD,position:'relative',margin:'15pt 15pt'}}
+                    >v 語音頻道
                 </typography>
+                <br/>
+
+                <div className='serverChannelRoom'>
+                    <typography
+                        component="h3" 
+                        variant="h3"
+                        style={{color: GRAY_WORD,margin:'15pt 15pt'}}
+                        ># 一般
+                    </typography>
+                </div> 
+                <br/>               
             </div>
         </div>
       );
